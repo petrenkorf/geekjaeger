d1b728b3fea722e40d022a2134a86f5ffa932ce9
Petris Rodrigo Fernandes <petrisrf@gmail.com>
2021-06-03 20:35:59 -0300

    Changed authentication routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '/'
   root to: 'login#index'
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

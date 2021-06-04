9402ecf39cd66ee1c40b4d49c8b9a88f3437ccf7
Petris Rodrigo Fernandes <petrisrf@gmail.com>
2021-06-03 21:09:46 -0300

    Authentication test

--- spec/system/authentication_spec.rb
+++ spec/system/authentication_spec.rb
@@ -1,10 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe 'User login', type: :system do
-  describe 'login page' do
-    it 'shows the right content' do
-      visit new_user_session_path
-      expect(page).to have_content('Log in')
-    end
+  scenario "with incorrect credentials" do
+    visit new_user_session_path
+
+    fill_in "user_email", with: 'petris@email.com'
+    fill_in "user_password", with: '12345678'
+
+    click_button "login_button"
+
+    expect(page).to have_current_path(new_user_session_path)
+    expect(page).to have_content("Invalid")
+  end
+
+  scenario "with correct credentials" do
+    visit new_user_session_path
+
+    fill_in "user_email", with: 'petris@email.com'
+    fill_in "user_password", with: '12345678'
+
+    click_button "login_button"
+
+    expect(page).to have_current_path(profile_url_path, url: true)
   end
 end
